95d5a1aa29e0fb2a991cf834040aed3cee31f6d4
Zaccal <ksss90411@gmail.com>
2025-04-24 13:30:08 +0300

    Revert "undo commit"
    This reverts commit 1b742a5363b0d0b855e1e2b15d572cae281b2ad2.

--- app/dashboard/latest-invoices.tsx
+++ app/dashboard/latest-invoices.tsx
@@ -2,21 +2,12 @@ import { ArrowPathIcon } from "@heroicons/react/24/outline";
 import clsx from "clsx";
 import Image from "next/image";
 import { LatestInvoice } from "@/app/lib/definitions";
-<<<<<<< HEAD:app/dashboard/latest-invoices.tsx
 import { SecondaryFont } from "../ui/font";
 import { fetchLatestInvoices } from "../lib/data";
 
 export default async function LatestInvoices() {
   const latestInvoices = await fetchLatestInvoices();
 
-=======
-import { SecondaryFont } from "../font";
-export default async function LatestInvoices({
-  latestInvoices,
-}: {
-  latestInvoices: LatestInvoice[];
-}) {
->>>>>>> parent of e4321bc (Complited a dashboard with fetching data):app/ui/dashboard/latest-invoices.tsx
   return (
     <div className="flex w-full flex-col md:col-span-4">
       <h2 className={`${SecondaryFont.className} mb-4 text-xl md:text-2xl`}>
@@ -24,16 +15,15 @@ export default async function LatestInvoices({
       </h2>
       <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
         {/* NOTE: Uncomment this code in Chapter 7 */}
-
-        {/* <div className="bg-white px-6">
+        <div className="bg-white px-6">
           {latestInvoices.map((invoice, i) => {
             return (
               <div
                 key={invoice.id}
                 className={clsx(
-                  'flex flex-row items-center justify-between py-4',
+                  "flex flex-row items-center justify-between py-4",
                   {
-                    'border-t': i !== 0,
+                    "border-t": i !== 0,
                   },
                 )}
               >
@@ -55,14 +45,14 @@ export default async function LatestInvoices({
                   </div>
                 </div>
                 <p
-                  className={`${lusitana.className} truncate text-sm font-medium md:text-base`}
+                  className={`${SecondaryFont.className} truncate text-sm font-medium md:text-base`}
                 >
                   {invoice.amount}
                 </p>
               </div>
             );
           })}
-        </div> */}
+        </div>{" "}
         <div className="flex items-center pb-2 pt-6">
           <ArrowPathIcon className="h-5 w-5 text-gray-500" />
           <h3 className="ml-2 text-sm text-gray-500 ">Updated just now</h3>
