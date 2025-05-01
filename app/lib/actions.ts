7c7dfd00e2690c570baac64488f5964ad0b33fb8
Adlet <ksss90411@gmail.com>
2025-05-01 10:29:08 +0300

    added a validation for create-form

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,22 +6,46 @@ import { redirect } from "next/navigation";
 
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(["pending", "paid"]),
+  customerId: z.string({
+    invalid_type_error: "Please select a customer",
+  }),
+  amount: z.coerce
+    .number()
+    .gte(1, { message: "Please enter an amount greater then $0" }),
+  status: z.enum(["pending", "paid"], {
+    invalid_type_error: "Please enter an invoice status",
+  }),
   date: z.string(),
 });
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
 const CreateInvoices = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoices(formData: FormData) {
-  const { amount, customerId, status } = CreateInvoices.parse({
+export async function createInvoices(prevState: State, formData: FormData) {
+  const validationFields = CreateInvoices.safeParse({
     customerId: formData.get("customerId"),
     amount: formData.get("amount"),
     status: formData.get("status"),
   });
+
+  if (!validationFields.success) {
+    return {
+      errors: validationFields.error.flatten().fieldErrors,
+      message: "Missing Fields. Fields to Create Invoice",
+    };
+  }
+
+  const { amount, customerId, status } = validationFields.data;
+
   const amountOfCents = amount * 100;
   const date = new Date().toISOString().split("T")[0];
 
@@ -31,7 +55,9 @@ export async function createInvoices(formData: FormData) {
     VALUES (${customerId}, ${amountOfCents}, ${status}, ${date})
   `;
   } catch (e) {
-    console.log(e);
+    return {
+      message: "Database Error: Failed to Create Invoice",
+    };
   }
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
